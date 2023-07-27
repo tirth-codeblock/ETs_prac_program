aa25c67ace4b47761e2dca01a1d592752de08bd2
tirth-codeblock <tirthgadhiya076@gmail.com>
2023-07-27 13:47:28 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,36 @@
+"""
+name:-Gadhiya Tirth
+Description :- make code for beaginer
+Date:-24/07/2023
+"""
+#type one
+
+print ("hello world")
+
+# type two (data type)
+
+a=13
+b=23.45
+c="Tirth"
+d="Gadhiya"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(a))
+
+# type three (indexing)
+print(c[2])
+
+#type four (slicing)
+
+print(d[0:5:1])
+
+
+#type five (boolean)
+
+print(bool(a))
+print(bool(d))
+
+#type six ()
+
