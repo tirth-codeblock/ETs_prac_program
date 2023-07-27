65c7625066865b1da396fcafe1242ca9ef9ec867
tirth-codeblock <tirthgadhiya076@gmail.com>
2023-07-27 13:52:06 +0530

    first commit

---
+++ task2.py
@@ -0,0 +1,13 @@
+#user input a character
+
+x=input("Enetr a character  : " )
+x=x.lower()
+
+if x=="a" :
+    print("you have 'a' ")
+elif x=="b" :
+    print("you have 'b' ")
+elif x=="c"  :
+    print("you have 'c' ")
+else :
+    print("Invavalid character")
